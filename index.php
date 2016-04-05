a57e82eac7a9b4fda05e943b3b24854d2655dc21
Jacob Muchow <jacobamuchow@gmail.com>
2016-04-05 16:40:54 -0500

    Refactored file locations to work for server.

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>HookedUp</title>
-    </head>
-<body>
-    <h1>HookedUp</h1>
-    <h3>Currently under construction</h3>
-</body>
+<?php
+
+header("Location: http://" . $_SERVER['HTTP_HOST'] . "/login.php");
