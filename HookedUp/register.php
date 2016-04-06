0540f36a0ced51051fb26a805069d6974961a6af
Jacob Muchow <jacobamuchow@gmail.com>
2016-04-06 12:10:00 -0500

    Updated register screen.

--- HookedUp/register.php
+++ HookedUp/register.php
@@ -3,13 +3,8 @@
 <head>
     <?php require 'links.php'; ?>
     <style>
-        .logo-top {
-            display: block;
+        .title {
             margin: 50px auto 0 auto;
-        }
-        .logo-bottom {
-            display: block;
-            margin: 0 auto 0 auto;
         }
     </style>
 </head>
@@ -18,38 +13,35 @@
 <?php require 'navbar.php'; ?>
 
 <div class="container">
-    <div class="logo"><div><img src="hooked.png" class="logo-top"></div>
-    <div><img src="up.png" class="logo-bottom"></div></div>
-        <div class="row">
-            <div class="col-md-6 col-md-offset-3">
-                <div class="panel panel-login">
-                    <hr>
-                    <div class="panel-body">
-                        <div class="row">
-                            <form id="login-form" action="http://phpoll.com/login/process" method="post" role="form" style="display: block;">
-                                <div class="col-lg-12">
-                                    <div class="form-group">
-                                        <input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
-                                    </div>
-                                    <div class="form-group text-center">
-                                        <input type="checkbox" tabindex="3" class="" name="remember" id="remember">
-                                        <label for="remember"> Remember Me</label>
-                                    </div>
+    <div class="title text-center">
+        <h1>Join HookedUp.</h1>
+        <h4><small>The best way to connect with professionals.</small></h4>
+    </div>
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <div class="panel panel-login">
+                <hr>
+                <div class="panel-body">
+                    <div class="row">
+                        <form id="login-form" action="http://phpoll.com/login/process" method="post" role="form" style="display: block;">
+                            <div class="col-lg-12">
+                                <div class="form-group">
+                                    <input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
                                 </div>
-                                <div class="col-lg-6 col-lg-offset-3">
-                                    <div class="form-group-lg">
-                                        <button type="submit" class="btn form-control btn-primary">Sign in</button>
-                                    </div>
-                                    <hr>
-                                    <div class="form-group text-center">
-                                        <h6>Not a member? <a href="register.php">Create an account.</a></h6>
-                                    </div>
+                                <div class="form-group">
+                                    <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
                                 </div>
-                            </form>
-                        </div>
+                            </div>
+                            <div class="col-lg-6 col-lg-offset-3">
+                                <div class="form-group-lg">
+                                    <button type="submit" class="btn form-control btn-primary">Create account</button>
+                                </div>
+                                <hr>
+                                <div class="form-group text-center">
+                                    <h6>Already have an account? <a href="login.php">Sign in.</a></h6>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
