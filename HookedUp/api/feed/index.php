0b1fab3684c79c828232b1846956f0722eb6b0c0
Brandon Erbschloe <berbschloe@gmail.com>
2016-04-17 17:11:00 -0500

    fix search

--- HookedUp/api/feed/index.php
+++ HookedUp/api/feed/index.php
@@ -1,6 +1,7 @@
 <?php
     require $_SERVER['DOCUMENT_ROOT'] . '/api/tools.php';
-        
+    $method = htmlspecialchars($_SERVER['REQUEST_METHOD']);
+
     switch ($method) {
         case 'GET':
             $query = "SELECT * FROM post ORDER BY createdAt DESC";
--- HookedUp/api/search/index.php
+++ HookedUp/api/search/index.php
@@ -1,18 +1,21 @@
 <?php
     require $_SERVER['DOCUMENT_ROOT'] . '/api/tools.php';
-        
+    $method = htmlspecialchars($_SERVER['REQUEST_METHOD']);
+
     switch ($method) {
         case 'GET':
             if (!$filter = getGETSafe('filter')) {
-                failure("filter required id");
+                failure("filter required");
             }
             
+            $filter = '%'.$filter.'%';
+            
             $query = "SELECT ".userProperties()." FROM user WHERE username LIKE ? OR firstName LIKE ? OR lastName LIKE ?";
-            $users = exec_stmt($query);
+            $users = exec_stmt($query, "sss", $filter, $filter, $filter);
            
             
             $query = "SELECT ".companyProperties()." FROM company WHERE name LIKE ?";
-            $companies = exec_stmt($query);
+            $companies = exec_stmt($query, "s", $filter);
             
             $result = array();
             
