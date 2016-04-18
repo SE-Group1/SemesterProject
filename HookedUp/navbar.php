cb22879f3404a0ac2206b6908a48624008ad9b13
Jacob Muchow <jacobamuchow@gmail.com>
2016-04-18 14:59:47 -0500

    Moved search bar into navbar.

--- HookedUp/navbar.php
+++ HookedUp/navbar.php
@@ -8,6 +8,12 @@
         <li><a href="#">Home</a></li>
         <li><a href="posts.php">Posts</a></li>
         <li><a href="Editprofile.php">Edit Profile</a></li>
+        <li><form action="" method="get">
+            Search:
+            <input type="search" name="filter" value = "<?php echo $filter ?>">
+            <input type="submit">
+        </form></li>
+        
         <!--<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Profile <span class="caret"></span></a>
           <ul class="dropdown-menu">
             <li><a href="#">Page 1-1</a></li>
--- HookedUp/search/index.php
+++ HookedUp/search/index.php
@@ -4,98 +4,69 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Search Results</title>
-</head>
-<body>
     <?php require '../links.php'; ?>
     <style>
-        
-            .btn btn-success {
-                align: right;
-                padding:20px;
-            }
-        
-            #nav {
-                line-height:30px;
-                background-color:#eeeeee;
-                height:1500px;
-                width:200px;
-                float:left;
-                padding:5px; 
-            }
-            #section {
-                width:1000px;
-                height:120px;
-                padding:10px; 
-            }   
-
+        .btn btn-success {
+            align: right;
+            padding:20px;
+        }
+    
+        #nav {
+            line-height:30px;
+            background-color:#eeeeee;
+            height:1500px;
+            width:200px;
+            float:left;
+            padding:5px; 
+        }
+        #section {
+            width:1000px;
+            height:120px;
+            padding:10px; 
+        }
     </style>
+</head>
+<body>
 <?php require '../navbar.php'; ?>
 <div id="nav">
-Location<br>
-Company<br>
-Date Posted<br>
-Industry<br>
-Experience Level<br>
-<button type="button" class="btn btn-success">Filter</button> 
+    Location<br>
+    Company<br>
+    Date Posted<br>
+    Industry<br>
+    Experience Level<br>
+    <button type="button" class="btn btn-success">Filter</button> 
 </div>
-<?php
-    if (!$filter = getGETSafe('filter')) { ?>
-        <div><center><h3>Need Search</h3></center></div>  
+<div><center><h3>Search Results</h3></center></div>
+
+<?php 
+    if(isset($filter)) {
+        $result = curl_get("api/search/index.php", array(
+            "filter" => $filter
+        ));
         
-        <form action="" method="get">
-            Search:
-            <input type="search" name="filter">
-            <input type="submit">
-        </form>
-              
-    <?php } else { ?>    
-        <div><center><h3>Search Result</h3></center></div>
+        $users = $result['result']['users'];
         
-        <form action="" method="get">
-            Search:
-            <input type="search" name="filter" value = "<?php echo $filter ?>">
-            <input type="submit">
-        </form>
-        
-        <?php 
-            $result = curl_get("api/search/index.php", array(
-                "filter" => $filter
-            ));
-            
-            $users = $result['result']['users'];
-            
-            foreach ($users as $user) { ?>  
-                <div id="section"> 
-                    <div class= "col-lg-2"><br><img src = "billGates.jpg" style = "width:100px;height:100px;"></div> 
-                    <div class= <h4>Profile Type: People</h4></div>
-                    <div class= <h4><?php echo $user['firstName'].' '.$user['lastName']?></h4></div>
-                    <div class= <small><?php echo $user['email']?></small></div>
-                    <div class= <sub><?php echo $user['phoneNumber']?></sub></div> 
-                    <button type="button" class="btn btn-success" onClick='/user'>View</button>  
-            </div> 
-            <?php }
-            
-            $companies = $result['result']['companies'];
-            foreach ($companies as $company) { ?>
-                <div id="section">  
+        foreach ($users as $user) { ?>  
+            <div id="section"> 
                 <div class= "col-lg-2"><br><img src = "billGates.jpg" style = "width:100px;height:100px;"></div> 
-                <div class= <h4>Profile Type: Company</h4></div>   
-                <div class= <h4><?php echo $company['name']?></h4></div>
-                <button type="button" class="btn btn-success" onClick='/company'>View</button>     
+                <div class= <h4>Profile Type: People</h4></div>
+                <div class= <h4><?php echo $user['firstName'].' '.$user['lastName']?></h4></div>
+                <div class= <small><?php echo $user['email']?></small></div>
+                <div class= <sub><?php echo $user['phoneNumber']?></sub></div> 
+                <button type="button" class="btn btn-success" onClick='/user'>View</button>  
             </div> 
-            <?php }
-            
-            /*for($i=0; $i < 10; $i++) { ?>
+        <?php }
+        
+        $companies = $result['result']['companies'];
+        foreach ($companies as $company) { ?>
             <div id="section">  
-                <div class= "col-lg-2"><br><img src = "web.png" alt= "Job Posting" style = "width:100px;height:100px;"></div>
-                <div class= <h4>Software Engineer</h4></div>
-                <div class= <small>Microsoft</small></div>
-                <div class= <sub>Greater Atlanta Area</sub></div> 
-                <button type="button" class="btn btn-success">View</button>     
-            </div>
-        <?php } ?>*/
-        } ?>
+            <div class= "col-lg-2"><br><img src = "billGates.jpg" style = "width:100px;height:100px;"></div> 
+            <div class= <h4>Profile Type: Company</h4></div>   
+            <div class= <h4><?php echo $company['name']?></h4></div>
+            <button type="button" class="btn btn-success" onClick='/company'>View</button>     
+        </div> 
+        <?php } ?>
+    <?php } ?>
     
 </body>
 </html>
