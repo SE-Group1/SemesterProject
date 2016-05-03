4eb965eeb831b628e7138171589c1500ca4b657d
Benjamin Seah <bjsz82@mail.missouri.edu>
2016-05-03 15:06:19 -0500

    User Feed

--- HookedUp/home.php
+++ HookedUp/home.php
@@ -1,5 +1,26 @@
 <?php require 'tools.php'; 
     requireLoggedIn();
+    $url = "/api/company/index.php";
+    $fields = array(
+        'id' => 'Company-1'
+    );
+    
+    $result = curl_get($url, $fields);
+    $company = $result['result'][0];
+    //echo json_encode($company);
+    
+    $field = array(
+        'companyId' => 'Company-1'
+    );
+    
+    $url1 = "/api/company/employees/index.php";
+    $results = curl_get($url1, $field);
+    $employees = $results['result'];
+    //echo json_encode($employees);
+    
+    $url2 = "/api/company/post.php";
+    $result2 = curl_get($url2, $field);
+    $posts = $result2['result'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -34,6 +55,21 @@
 <div class="container">
     <p id="result">
     </p>
+    <div class = "recent_posts">
+                    <h2>Recent Posts</h2>
+                    <hr>
+                    
+                    <?php
+                        foreach($posts as $post) { ?>
+                            <div><img src="Papajohns.jpeg" class="col-md-4 img-thumbnail postpic" >
+                            <div class="col-md-6"><?php echo $post['comment'] ?> </div>
+                            <div class="name"><h5><b><?php echo $post['createdAt']?></b></h5></div>
+                            <div class='clearfix'></div>
+                            </div><hr>
+                        <?php }
+                    ?>   
+                    
+                </div>
 </div>
 
 </body>
